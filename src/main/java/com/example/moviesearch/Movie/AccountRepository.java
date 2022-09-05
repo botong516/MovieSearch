87dd1224c327a3cb1de5caf28c101de0a7b6e483
ByteDance <ByteDance@xuqipengdeMacBook-Pro.local>
2022-09-05 17:17:41 +0800

    feat: add .gitignore file

--- src/main/java/com/example/moviesearch/Movie/AccountRepository.java
+++ src/main/java/com/example/moviesearch/Movie/AccountRepository.java
@@ -1,6 +1,5 @@
 package com.example.moviesearch.Movie;
 
-import javafx.scene.chart.Axis;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
--- src/main/java/com/example/moviesearch/Movie/Query.java
+++ src/main/java/com/example/moviesearch/Movie/Query.java
@@ -1,5 +1,5 @@
 package com.example.moviesearch.Movie;
 
 public class Query {
-
+    
 }
