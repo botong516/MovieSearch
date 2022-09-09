ef4f52288e12770ee557ac019b8a571ee13394cf
ByteDance <xuqipeng@bytedance.com>
2022-09-09 13:49:26 +0800

    feat: add token

--- src/main/java/com/example/movie/constant/IMDBConstant.java
+++ src/main/java/com/example/movie/constant/IMDBConstant.java
@@ -1,7 +1,7 @@
 package com.example.movie.constant;
 
 public class IMDBConstant {
-    public static final String Key  = "k_5yhf5s1y";  // k_8kmyy76b k_womok0b9  k_z4y6pc1i k_5yhf5s1y
+    public static final String Key  = "k_womok0b9";  // k_8kmyy76b k_womok0b9  k_z4y6pc1i  k_5yhf5s1y
     public static final String TOP250MOVIES = "https://imdb-api.com/en/API/Top250Movies/"+ Key;;
     public static final String SEARCH = "https://imdb-api.com/en/API/SearchMovie/"+Key+"/";
     public static final String MOSTPOPULAR = "https://imdb-api.com/en/API/MostPopularMovies/"+Key;
